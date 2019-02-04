931c0e2fb56e3e571c4db6a89c981144314c1377
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-02-04 16:12:45 +0530

    Create count_prime_9.py

---
+++ count_prime_9.py
@@ -0,0 +1,12 @@
+l,r=map(int,input().split())
+count=0
+for i in range(l,r+1):
+    if i==2:
+        count+=1
+    else:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            count+=1
+print(count)
