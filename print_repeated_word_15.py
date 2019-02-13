7355a3b3b35c8fe5e0c9702a78b17cbe44fc4f0e
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-02-13 15:54:50 +0530

    Create print_repeated_word_15.py

---
+++ print_repeated_word_15.py
@@ -0,0 +1,13 @@
+str1=input()
+dic={}
+max=0
+for i in str1:
+  if i!=" ":
+    dic[i]=str1.count(i)
+    if dic[i]>max:
+      max=dic[i]
+for key in dic:
+  if dic[key]==max:
+    print(key)
+    break
+  
