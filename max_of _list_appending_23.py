fd91a7035f09c8efee46bdb00dc3a56198a94150
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-02-14 14:22:15 +0530

    Create max_of _list_appending_23.py

---
+++ max_of _list_appending_23.py
@@ -0,0 +1,8 @@
+n,k=map(int,input().split())
+li1=list(map(int,input().split()))
+li2=list(map(int,input().split()))
+li3=[]
+for i in range(len(li2)):
+  li1.append(li2[i])
+  li3.append(max(li1))
+print(*li3)
