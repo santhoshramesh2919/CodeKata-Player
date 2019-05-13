dea4ddb0e8039c70a8c788229148460a5fc38b3c
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-13 14:15:41 +0530

    Create powerOf2.py

---
+++ powerOf2.py
@@ -0,0 +1,12 @@
+#Power of 2
+def isPowerOf2(n):
+    if n==0:
+        return True
+    while(n!=1):
+        if (n%2!=0):
+            return False
+        n=n//2
+    return True
+
+n=int(input())
+print("yes" if isPowerOf2(n) else "no")
