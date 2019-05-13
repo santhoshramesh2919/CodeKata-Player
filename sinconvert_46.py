cbdf7e6ee42f212bb75243a73aec4522b9de80bd
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-13 15:35:32 +0530

    Update sinconvert_46.py

--- sinconvert_46.py
+++ sinconvert_46.py
@@ -2,4 +2,11 @@ import math
 
 n=int(input())
 rad=math.radians(n)
-print(round(math.sin(rad),1))
+val=round(math.sin(rad),1)
+if n==90:
+    print("1")
+    
+elif val==0:
+    print("0")
+else:
+    print(val)
