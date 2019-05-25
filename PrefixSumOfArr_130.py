97260e6605579d2b215277baeeeb68308abf296c
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-25 20:43:29 +0530

    Update PrefixSumOfArr_130.py

--- PrefixSumOfArr_130.py
+++ PrefixSumOfArr_130.py
@@ -2,9 +2,8 @@ n=int(input())
 li=list(map(int,input().split()))
 op=[]
 for i in range(len(li)):
-  if li[i]%2==0:
+  if i%2==1:
     op.append(sum(li[:i+1]))
   else:
     op.append(li[i])
 print(*op)
-
