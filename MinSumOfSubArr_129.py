436072abd9538f20f2fb0b2c9348ee62357a9321
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-25 20:33:24 +0530

    Update MinSumOfSubArr_129.py

--- MinSumOfSubArr_129.py
+++ MinSumOfSubArr_129.py
@@ -1,9 +1,12 @@
 n=int(input())
 li=list(map(int,input().split()))
-m=li[0]
-for i in range(len(li)-1):
-  for j in range(i+1,len(li)):
-    s=sum(li[i:j+1])
-    if m>s:
-      m=s
+m=0
+if len(li)==1:
+  m=li[0]
+else:
+  for i in range(len(li)-1):
+    for j in range(i+1,len(li)):
+      s=sum(li[i:j+1])
+      if m>s:
+        m=s
 print(m)
