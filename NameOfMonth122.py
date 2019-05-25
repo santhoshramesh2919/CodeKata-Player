9867645428acfbb40e399dedaa65065f54db8c06
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-25 19:09:52 +0530

    Update NameOfMonth122.py

--- NameOfMonth122.py
+++ NameOfMonth122.py
@@ -1,25 +1,25 @@
-d,m,y=map(int,input().split("-"))
-if str(m)=='01':
+d,m,y=map(str,input().split("-"))
+if m=='01':
   print("January")
-elif str(m)=="02":
+elif m=="02":
   print("February")
-elif str(m)=='03':
+elif m=='03':
   print("March")
-elif str(m)=='04':
+elif m=='04':
   print("April")
-elif str(m)=='05':
+elif m=='05':
   print("May")
-elif str(m)=='06':
+elif m=='06':
   print("June")
-elif str(m)=='07':
+elif m=='07':
   print("July")
-elif str(m)=='08':
+elif m=='08':
   print("August")
-elif str(m)=='09':
+elif m=='09':
   print("September")
-elif str(m)=='10':
+elif m=='10':
   print("October")
-elif str(m)=='11':
+elif m=='11':
   print("November")
-elif str(m)=='12':
+elif m=='12':
   print("December")
