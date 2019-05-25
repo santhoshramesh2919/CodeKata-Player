0c06ae1ae13e148c4bee2618bf6d7cc29d704bd1
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-25 12:33:48 +0530

    Update DateFormat_113.py

--- DateFormat_113.py
+++ DateFormat_113.py
@@ -1,5 +1,5 @@
 li=list(map(int,input().split("/")))
-if li[0]<31 and len(str(li[0]))==2 and len(str(li[1]))==2 and li[1]<12 and len(str(li[2]))==4:
+if li[0]<31 and len(str(li[0]))==2 and len(str(li[1]))==2 and li[1]<13 and len(str(li[2]))==4:
   print("yes")
 else:
   print("no")
